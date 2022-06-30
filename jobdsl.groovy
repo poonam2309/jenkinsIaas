d0de7726cadccaf50bebc9b72d2f827b6c4fafb5
poonam2309 <67383223+poonam2309@users.noreply.github.com>
2022-06-30 19:56:15 +0530

    Update jobdsl.groovy

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -18,6 +18,24 @@ job('cloudrundeploy') {
             }
             referencedParameter('PROJECT')
         }
+        activeChoiceReactiveReferenceParam('VPC_CONNECTOR') {
+            description('Allows user choose from multiple choices')
+            omitValueField()
+            choiceType('FORMATTED_HIDDEN_HTML')
+            groovyScript {
+                script('def vpc1= "usc1-kcfn01-sss-cor-cnc01"
+                      def vpc2= "use1-kcfn01-sss-cor-cnc01"
+                      if(PROJECT=="gcp-kcfn01" && REGION=="us-central1")
+                      return "<b>${vpc1}</b><input type=\"hidden\" name=\"value\" value=\"${vpc1}\" />"
+                      else if (PROJECT=="gcp-kcfn01" && REGION=="us-east1")
+                      return "<b>${vpc2}</b><input type=\"hidden\" name=\"value\" value=\"${vpc2}\" />"
+                      else 
+                      return "Kindly select valid Project and its Respective Region"')
+                fallbackScript('"fallback choice"')
+            }
+            referencedParameter('PROJECT')
+            referencedParameter('REGION')
+        }
             
     }
 }
