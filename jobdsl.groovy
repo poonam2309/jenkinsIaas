30ff872fcdb9d2b18bfe2b18d8e143568821c6f8
poonam2309 <67383223+poonam2309@users.noreply.github.com>
2022-07-01 13:18:05 +0530

    Update jobdsl.groovy

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -23,13 +23,13 @@ job('cloudrundeploy') {
             omitValueField()
             choiceType('FORMATTED_HIDDEN_HTML')
             groovyScript {
-                script('''def vpc1= "usc1-kcfn01-sss-cor-cnc01" \r\n
-                      def vpc2= "use1-kcfn01-sss-cor-cnc01" \r\n
-                      if(PROJECT=="gcp-kcfn01" && REGION=="us-central1") \r\n
-                      return "<b>${vpc1}<\/b><input type=\"hidden\" name=\"value\" value=\"${vpc1}\" \/>" \r\n
-                      else if (PROJECT=="gcp-kcfn01" && REGION=="us-east1") \r\n
-                      return "<b>${vpc2}<\/b><input type=\"hidden\" name=\"value\" value=\"${vpc2}\" \/>" \r\n
-                      else  \r\n
+                script('''def vpc1= "usc1-kcfn01-sss-cor-cnc01" 
+                      def vpc2= "use1-kcfn01-sss-cor-cnc01" 
+                      if(PROJECT=="gcp-kcfn01" && REGION=="us-central1") 
+                      return "<b>${vpc1}</b><input type=\"hidden\" name=\"value\" value=\"${vpc1}\" />" 
+                      else if (PROJECT=="gcp-kcfn01" && REGION=="us-east1") 
+                      return "<b>${vpc2}</b><input type=\"hidden\" name=\"value\" value=\"${vpc2}\" />" 
+                      else  
                       return "Kindly select valid Project and its Respective Region"''') 
                 fallbackScript('"fallback choice"')
             }
