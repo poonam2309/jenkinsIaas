b2143bfd18def912bf4fbf858a43f9acd12a2621
poonam2309 <67383223+poonam2309@users.noreply.github.com>
2022-07-04 13:12:21 +0530

    Update jobdsl.groovy

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -1,6 +1,5 @@
 job('cloudrundeploy') {
     parameters {
-        choiceParam('myParameterName', ['option 1 (default)', 'option 3', 'option 5'], 'my description')
         activeChoiceParam('PROJECT') {
             description('Allows user choose from multiple choices')
             choiceType('SINGLE_SELECT')
@@ -13,7 +12,12 @@ job('cloudrundeploy') {
             description('Allows user choose from multiple choices')
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script('if(PROJECT=="gcp-kcfn01") return["us-central1","us-east2" ] else if (PROJECT=="gcp-cust01") return["us-east1"] else return ["null"]')
+                script('''if(PROJECT=="gcp-kcfn01") 
+                          return["us-central1","us-east2" ] 
+                          else if (PROJECT=="gcp-cust01") 
+                          return["us-east1"] 
+                          else 
+                          return ["null"]''')
                 fallbackScript('"fallback choice"')
             }
             referencedParameter('PROJECT')
