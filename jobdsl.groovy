5fffb144e3f96229a09bf52084c91984e2f767af
poonam2309 <67383223+poonam2309@users.noreply.github.com>
2022-07-04 18:24:09 +0530

    Update jobdsl.groovy

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -66,6 +66,9 @@ pipelineJob('cloud_automation/terraform/cloudrun/cloudrundeploy1') {
             referencedParameter('PROJECT')
             referencedParameter('REGION')
         }
+      stringParam('RELEASE_NUMBER', '08.05.00', 'Release Version')
+      stringParam('INVOKERIMAGE_TAG', '201', 'Invoker image Version')
+      stringParam('RUNNERIMAGE_TAG', '202', 'Runner image Version')
             
     }
   /*  scm 
