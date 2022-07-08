d9459ebb7e077edb449cb1062a0b736e6ec221cd
poonam2309 <67383223+poonam2309@users.noreply.github.com>
2022-07-08 15:59:51 +0530

    Update sandbox.groovy

--- sandbox.groovy
+++ sandbox.groovy
@@ -46,4 +46,33 @@ pipelineJob('sandbox') {
   randomName('')
   filterLength(0)
 }
+      cascadeChoiceParameter {
+            name('VPC_CONNECTOR')
+      filterable(false)
+            description('Allows user choose from multiple choices')
+            choiceType('ET_FORMATTED_HTML')
+      script{
+            groovyScript {
+             script {
+                 script('''def vpc1= "usc1-kcfn01-sss-cor-cnc01" 
+                      def vpc3= "use1-kcfn01-sss-cor-cnc01" 
+                      if(PROJECT=="gcp-kcfn01" && REGION=="us-central1") 
+                      return  "<b>${vpc1}</b><input type='hidden' name='value' value='${vpc1}'>"
+                      
+                      else if (PROJECT=="gcp-kcfn01" && REGION=="us-east1") 
+                      return "<b>${vpc3}</b><input type='hidden' name='value' value='${vpc3}'>" 
+                      else  
+                      return "no match condition exist"''') 
+                sandbox(true)
+             }
+               fallbackScript {
+        script("return ['Unable to list branches']")
+        sandbox(true)
+      }
+            }
+            referencedParameters('PROJECT','REGION')
+        }
+  randomName('')
+  filterLength(0)
+}
    }}
