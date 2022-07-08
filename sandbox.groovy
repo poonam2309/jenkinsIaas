7f1ca00f3dffe6784f7322ccc085cf9c0e32ba37
poonam2309 <67383223+poonam2309@users.noreply.github.com>
2022-07-08 16:14:28 +0530

    Update sandbox.groovy

--- sandbox.groovy
+++ sandbox.groovy
@@ -57,10 +57,10 @@ pipelineJob('sandbox') {
                  script('''def vpc1= "usc1-kcfn01-sss-cor-cnc01" 
                       def vpc3= "use1-kcfn01-sss-cor-cnc01" 
                       if(PROJECT=="gcp-kcfn01" && REGION=="us-central1") 
-                      return  "<b>${vpc1}</b><input type='hidden' name='value' value='${vpc1}'>"
+                      return "<input class='setting-input' name='value' type='text' value='${vpc1}'>"
                       
                       else if (PROJECT=="gcp-kcfn01" && REGION=="us-east1") 
-                      return "<b>${vpc3}</b><input type='hidden' name='value' value='${vpc3}'>" 
+                      return "<input class='setting-input' name='value' type='text' value='${vpc3}'>" 
                       else  
                       return "no match condition exist"''') 
                 sandbox(true)
